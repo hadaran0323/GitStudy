6db822736146a5a4e969642250a8878a45901f75
hadaran0323 <s.s.hadaran@gmail.com>
2023-06-17 12:29:26 +0900

    引き算の機能を追加

--- add.py
+++ add.py
@@ -1,8 +1,12 @@
 def add(a, b):
     return a + b
 
+def sub(a, b):     #追加
+    return a - b   #追加
+
 def main():
     print(add(4, 2))
+    print(sub(6, 2))  #追加
     
 if __name__  == '__main__':
     main()
